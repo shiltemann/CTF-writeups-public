7ef59a9449dcd986d216401b148a30e2c6dc6ad6
Saskia Hiltemann <zazkia@gmail.com>
2018-10-07 13:47:47 +0200

    updates

--- _resources/code/crypto/rsa.py
+++ _resources/code/crypto/rsa.py
@@ -1,33 +1,19 @@
 import argparse
 import gmpy2
-import primefac
 from Crypto.PublicKey import RSA
 from fractions import gcd
 
-def egcd(a, b):
-    if a == 0:
-        return (b, 0, 1)
-    else:
-        g, y, x = egcd(b % a, a)
-        return (g, x - (b // a) * y, y)
 
-def modinv(a, m):
-    g, x, y = egcd(a, m)
-    if g != 1:
-        raise Exception('modular inverse does not exist')
-    else:
-        return x % m
-
-
-### modular exponentiation a^b % N
-# find ct given pt,e,N ( pt^e mod N )
-# find pt given ct,d,N ( ct^d mod N )
 def modexp(a, b, N):
+    """ modular exponentiation a^b % N
+    find ct given pt,e,N ( pt^e mod N )
+    find pt given ct,d,N ( ct^d mod N )
+    """
     return gmpy2.powmod(a, b, N)
 
 
-# without using libs
 def modexp2(a, b, N):
+    """ modular exponentiation without using libs """
     r = 1
     while 1:
         if b % 2 == 1:
@@ -38,41 +24,55 @@ def modexp2(a, b, N):
         a = a * a % N
     return r
 
-### modular division
-# find
-def moddiv(a, b, N):
-    gmpy2.lo
 
-def compute_totient(p, q):
+def moddiv(a, b, N):
+    """ modular division
+    find d given e, p, q -> gmpy2.divm(1,e,totient(p,q))
+    """
+    gmpy2.divm(a, b, N)
+
+
+def totient(p, q):
+    """totient
+    find r given p,q
+    """
     return (p-1)*(q-1)
 
-def factor(N):
-    return primefac.primefac(N)
 
-### helpers
+def factor(N):
+    """ factorization
+    TODO: try factordb first: https://github.com/ryosan-470/factordb-pycli
+    if failes, try to compute ?
+    """
+    pass
+
+
+# helpers
 def modulus_from_factors(p, q):
+    """ multiplication
+    find N given p, q
+    """
     return p * q
 
-### division
-# find p given N, q
-# find q given N, p
-def factor_from_modulus_and_factor(f, N):
-    return int(N / f)
 
+def factor_from_modulus_and_factor(f, N):
+    """ division
+    find p given N, q
+    find q given N, p
+    """
+    return N // f
 
 
 def test():
     assert (modexp(2, 3, 10) == 8), "problem with modexp function"
     assert (modexp(3, 2, 10) == 9), "problem with modexp function"
     assert (modexp(3, 3, 10) == 7), "problem with modexp function"
-
-    assert (modulus_from_factors(93187,94603) == 8815769761), "problem with modulus from factors function"
+    assert (modulus_from_factors(93187, 94603) == 8815769761), "problem with modulus from factors function"
 
 
 # pip install gmpy2
 #   - sudo apt-get install libmpfr-dev
 #   - sudo apt-get install libmpc-dev
-# pip install primefac
 # if no target selected, all calculatable variables will be printed to stdout
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='RSA related computations')
@@ -88,5 +88,17 @@ if __name__ == '__main__':
 
     args = vars(parser.parse_args())
 
-    test()
+    """
+    N = p*q
+    p = N // q
+    q = N // p
 
+    r = (q-1)*(p-1)
+
+    d = gmpy2.divm(1,e,r)
+    e = gmpy2.divm(1,d,r)
+
+    ct = gmpy2.powmod(pt,e,N)
+    pt = gmpy2.powmod(ct,d,N)
+    """
+    test()
