b8cfc98db492a06ecbf2a6b4761aa7f7d2e32959
Saskia Hiltemann <zazkia@gmail.com>
2018-10-07 16:41:14 +0200

    tweaks

--- _resources/code/stegano/extractlsb.py
+++ _resources/code/stegano/extractlsb.py
@@ -1,5 +1,6 @@
 import argparse
 import binascii
+from pathlib import Path
 from PIL import Image
 
 
@@ -15,13 +16,13 @@ def int2bytes(i):
 
 
 def extract_lsb(inputimage, text, invert, decode):
+    outfilename = Path(inputimage).resolve().stem
     img = Image.open(inputimage)
-    img = img.convert('RGB')
+    img = img.convert('RGB')  # TODO: add support for alpha channel?
     pixels = img.load()
 
     (w, h) = img.size
     print(w, h)
-    #h = 3
 
     background = "white" if invert else "black"
     foreground = (0, 0, 0) if invert else (255, 255, 255)
@@ -39,9 +40,8 @@ def extract_lsb(inputimage, text, invert, decode):
 
     for j in range(0, h):
         if text or decode:
-            for i in range(0,3):
+            for i in range(0, 3):
                 pixels_txt[i] += '\n'
-            #pixels_txt = [x[0] + x[1] for x in zip(pixels_txt, ('\n', '\n', '\n'))]
         for i in range(0, w):
             (r, g, b) = pixels[i, j]
             if r & 1:
@@ -51,25 +51,24 @@ def extract_lsb(inputimage, text, invert, decode):
             if b & 1:
                 pixels_b[i, j] = foreground
             if text or decode:
-                pixels_txt[0] += str(r & 1) if not invert else str(not(r&1))
-                pixels_txt[1] += str(g & 1) if not invert else str(not(g&1))
-                pixels_txt[2] += str(b & 1) if not invert else str(not(b&1))
-                #pixels_txt = [x[0] + x[1] for x in zip(pixels_txt, (str(r & 1 & invert), str(g & 1 & invert), str(b & 1 & invert)))]
+                pixels_txt[0] += str(r & 1) if not invert else str((r & 1) ^ 1)
+                pixels_txt[1] += str(g & 1) if not invert else str((g & 1) ^ 1)
+                pixels_txt[2] += str(b & 1) if not invert else str((b & 1) ^ 1)
 
-    outimg_r.save("outimg_r.png")
-    outimg_g.save("outimg_g.png")
-    outimg_b.save("outimg_b.png")
+    outimg_r.save(outfilename + "_lsb_r.png")
+    outimg_g.save(outfilename + "_lsb_g.png")
+    outimg_b.save(outfilename + "_lsb_b.png")
 
     if text:
-        with open('outimg_r.txt', 'w') as fr, open('outimg_g.txt', 'w') as fg, open('outimg_b.txt', 'w') as fb:
+        with open(outfilename + '_lsb_r.txt', 'w') as fr, open(outfilename + '_lsb_g.txt', 'w') as fg, open(outfilename + '_lsb_b.txt', 'w') as fb:
             fr.write(pixels_txt[0].lstrip())
             fg.write(pixels_txt[1].lstrip())
             fb.write(pixels_txt[2].lstrip())
 
     # convert the bitstring to bytes and save
     if decode:
-        padsize = 8 - len(pixels_txt[0].replace('\n','')) % 8
-        with open('outimg_r.bin', 'wb') as fr, open('outimg_g.bin', 'wb') as fg, open('outimg_b.bin', 'wb') as fb:
+        # padsize = 8 - len(pixels_txt[0].replace('\n', '')) % 8
+        with open(outfilename + '_lsb_r.bin', 'wb') as fr, open(outfilename + '_lsb_g.bin', 'wb') as fg, open(outfilename + '_lsb_b.bin', 'wb') as fb:
             fr.write(text_from_bits(pixels_txt[0].replace('\n', '')))
             fg.write(text_from_bits(pixels_txt[1].replace('\n', '')))
             fb.write(text_from_bits(pixels_txt[2].replace('\n', '')))
