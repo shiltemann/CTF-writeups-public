cc3d90db5826a494bd5beae2d1514f2235954069
Helena Rasche <hxr@hx42.org>
2018-10-13 23:12:39 +0200

    move into lib

---
+++ PicoCTF_2018/writeupfiles/netcat.py
@@ -0,0 +1,39 @@
+import socket
+
+class Netcat:
+    """
+    Python 'netcat like' module
+    https://gist.github.com/leonjza/f35a7252babdf77c8421
+    """
+
+    def __init__(self, ip, port):
+
+        self.buff = ""
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((ip, port))
+
+    def read(self, length = 1024):
+
+        """ Read 1024 bytes off the socket """
+
+        return self.socket.recv(length)
+
+    def read_until(self, data):
+
+        """ Read data into the buffer until we have data """
+
+        while not data in self.buff:
+            self.buff += self.socket.recv(1024)
+
+        pos = self.buff.find(data)
+        rval = self.buff[:pos + len(data)]
+        self.buff = self.buff[pos + len(data):]
+
+        return rval
+
+    def write(self, data):
+
+        self.socket.send(data)
+
+    def close(self):
+        self.socket.close()
