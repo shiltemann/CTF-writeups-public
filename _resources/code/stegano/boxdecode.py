493d9d52419b6f301d4a21cc83ed94124b51f425
Helena Rasche <hxr@hx42.org>
2018-12-08 10:40:36 +0100

    cleanup

--- _resources/code/stegano/boxdecode.py
+++ _resources/code/stegano/boxdecode.py
@@ -6,14 +6,24 @@ from PIL import Image, ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 
-def doit(inputimage, width):
-    outfilename = Path(inputimage).resolve().stem
+maps = {
+    'black0': {
+        (0, 0, 0): 0,
+        (255, 255, 255): 1,
+    },
+    'black1': {
+        (0, 0, 0): 1,
+        (255, 255, 255): 0,
+    }
+}
+
+
+def doit(inputimage, width, cmap='black0'):
     img = Image.open(inputimage)
-    img = img.convert('RGB')  # TODO: add support for alpha channel?
+    img = img.convert('RGB')
     pixels = img.load()
 
     (w, h) = img.size
-    print(w, h)
 
     wo = w / width
     ho = h / width
@@ -23,21 +33,21 @@ def doit(inputimage, width):
             ii = int(i * wo + wo / 2)
             jj = int(j * ho + ho / 2)
 
-            (r, g, b) = pixels[ii, jj]
+            rgb = pixels[ii, jj]
 
-            if (r, g, b) == (0, 0, 0):
-                sys.stdout.write('0')
-            elif (r, g, b) == (255, 255, 255):
-                sys.stdout.write('1')
-            else:
-                sys.stdout.write('?')
+            # TODO: support approximate colours + grabbing a set of pixels
+            # around and averaging them for better detection rates.
+            q = maps[cmap].get(rgb, '?')
+            sys.stdout.write(str(q))
+
         sys.stdout.write('\n')
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Extract LSB from each channel of an image')
+    parser = argparse.ArgumentParser(description='Convert a square PNG that is a matrix of black/white squares into a 2d array of 1/0s')
     parser.add_argument('image', help='Input image')
     parser.add_argument('width', type=int, help='number of expected boxes in X')
+    parser.add_argument('--cmap', choices=['black0', 'black1'], help='Decoding map', default='black1')
     args = parser.parse_args()
 
-    doit(args.image, args.width)
+    doit(args.image, args.width, args.cmap)
