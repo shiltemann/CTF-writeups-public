c88fc695d9c90ff1f2d02f9488444438847babf7
Saskia Hiltemann <zazkia@gmail.com>
2023-04-24 16:34:57 +0200

    more annotations and points field

--- bin/create-writeup.sh
+++ bin/create-writeup.sh
@@ -8,10 +8,11 @@ echo "---
 layout: writeup
 
 title:
-level:  # optional, for events that use levels
-difficulty:
-categories: []
-tags: []
+level:          # optional, for events that use levels (like HackyEaster)
+difficulty:     # easy/medium/hard etc, if applicable
+points:         # if used
+categories: []  # e.g. crypto, pwn, reversing
+tags: []        # anything notable about challenge/solution, vuln/tools/etc
 
 flag:
 
