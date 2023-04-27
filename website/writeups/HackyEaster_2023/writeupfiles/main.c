6b0c392dba984b7029269f72d611600c8f7b9516
Saskia Hiltemann <zazkia@gmail.com>
2023-04-27 12:27:44 +0200

    add description

--- website/writeups/HackyEaster_2023/writeupfiles/main.c
+++ website/writeups/HackyEaster_2023/writeupfiles/main.c
@@ -1,33 +1,3 @@
----
-layout: writeup
-
-title: Thumper's PWN 2
-level: 7     # optional, for events that use levels (like HackyEaster)
-difficulty: hard     # easy/medium/hard etc, if applicable
-points: 300        # if used
-categories: [pwn]  # e.g. crypto, pwn, reversing
-tags: []        # anything notable about challenge/solution, vuln/tools/etc
-
-flag:
-
----
-
-## Challenge
-
-Thumper got one step closer to Dr. Evil but there's still a lot he has to learn. That's why he's practicing the ancient
-art of ROP. Help him solve this challenge by reading the file FLAG, so he can be on his way.
-
-Target: `nc ch.hackyeaster.com 2314`
-
-Note: The service is restarted every hour at x:00.
-
-[thumperspwn2.zip](writeupfiles/thumperspwn2.zip)
-
-## Solution
-
-We get a `main.c` file:
-
-```c
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -89,5 +59,4 @@ void main() {
 
   vuln();
 }
-```
-{: file=main.c}
+
